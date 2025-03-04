1228e7f7fafe586998d266aa30525924210542e6
Lakshan Damayantha <damayanthalakshan8@gmail.com>
2025-03-04 14:21:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,7 +43,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Manu-MD&xakAnYKJ#FLwjeT_VKc_3YThmwSPlsuIOdZGiuVGcuPb8ludc4TM' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'Manu-MD&ICNzyYaY#OtKh4NxGDV-KK5ZQlgaylPHr44Nj22k021CNSL0siko' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SUDO: process.env.SUDO === undefined ? '94742274855' : process.env.SUDO,
